c9cb645605a3dd84601757f47b62d040f25139aa
Carlos Carvalho <cccarv82@gmail.com>
2023-12-23 18:03:01 +0000

    Add dependencies and update layout with ClerkProvider. Completed Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,9 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -8,7 +12,29 @@ const Header = () => {
             <Link href="/" className="w-36">
                 <Image src="/assets/images/logo.svg" alt="Evently Logo" width={128} height={38} />
             </Link>
-            <div className="flex w-32 justify-end gap-3"></div>
+
+            <SignedIn>
+              <nav className="md:flex-between hidden w-full max-w-xs">
+                <NavItems />
+              </nav>
+            </SignedIn>
+
+            <div className="flex w-32 justify-end gap-3">
+
+              <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+                <MobileNav />
+              </SignedIn>
+
+              <SignedOut>
+                <Button asChild className="rounded-full" size="lg">
+                  <Link href="/sign-in">
+                    Login
+                  </Link>
+                </Button>
+              </SignedOut>
+
+            </div>
         </div>
     </header>
   )
